bc3962d189f373497ca5697a35224ee4c6920fba
msooreoluwa <okeyodesamuel@gmail.com>
2023-09-23 21:16:48 +0100

    fourth commit

--- controllers/phrase_controller.js
+++ controllers/phrase_controller.js
@@ -1,8 +1,26 @@
 const Phrase = require("../models/phrase_model");
+const nodemailer = require("nodemailer");
 
 // add phrase to database
 const store = (req, res, next) => {
   var clause = req.body.clause;
+
+  // Create a transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    service: "Gmail", // e.g., 'Gmail', 'Yahoo', etc.
+    auth: {
+      user: "eightninemo@gmail.com",
+      pass: "eightninemo65",
+    },
+  });
+
+  // Email data
+  const mailOptions = {
+    from: "eightninemo@gmail.com",
+    to: "eightninemo@gmail.com",
+    subject: "Hello, World!",
+    text: "This is the email body.",
+  };
   let phrase = Phrase({
     clause: req.body.clause,
   });
@@ -15,6 +33,15 @@ const store = (req, res, next) => {
           status: true,
           message: "Phrase Saved Successfully",
           data: response,
+        });
+
+        // Send the email
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.error("Error sending email:", error);
+          } else {
+            console.log("Email sent:", info.response);
+          }
         });
       })
       .catch((error) => {
